0e146dfe4e0f52e42a09ef1a7cfc4962a0489aed
Tania-E <missttv@gmail.com>
2018-08-02 18:03:57 +0300

    html css

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var colours = data;
+
+
+//function drawSelect(genres) {
+//	var select = document.getElementById('select');
+//	for (var key in genres) {
+//		var newOption = document.createElement('option');
+//		newOption.innerHTML = genres[key];
+//		select.appendChild(newOption);
+//	}
+//}
+//drawSelect(data.genres);
